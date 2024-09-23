403d6fd5f1c1f4456f6b46ef3e56eb5b904318b2
CodeCara <149534506+CodeCara@users.noreply.github.com>
2024-09-23 07:26:39 +0200

    New book button and basic form set-up

---
+++ script.js
@@ -0,0 +1,23 @@
+const myLibrary = ['Book1', 'Book2'];
+
+function myBook () {
+
+
+
+}
+
+function addToLibrary ()
+{
+
+
+
+}
+
+function displayBook () {
+for (let i=0; i<myLibrary.length; i++)
+console.table(myLibrary[i]);
+
+
+}
+
+displayBook();
