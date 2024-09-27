db8bd0f8b98e43f203c2be42801f73f085c17879
CodeCara <149534506+CodeCara@users.noreply.github.com>
2024-09-27 08:42:36 +0200

    Inputs captured and assigned to Book object.  This is draft code that needs to be modified

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const myLibrary = ['Book1', 'Book2'];
 const newBookButton = document.getElementById('newBookButton');
 const form = document.querySelector('form');
-const submitButton = document.querySelector('.submitButton');
+const addToLibraryButton = document.querySelector('.addToLibraryButton');
+
 
 
 
@@ -10,15 +11,32 @@ function revealForm () {
 
 newBookButton.addEventListener('click', revealForm);
 
+function captureInputs (){
+    let author = document.getElementById('author').value;
+
+    let title = document.getElementById('title').value;
+    let numberOfPages = document.getElementById('numberOfPages').value;
+    let ifRead =   document.getElementById('ifRead').value;
+    const Book1 = new Book (author, title, numberOfPages, ifRead); 
+    console.log(Book1);
+
+
+
+
+
+
+}
+
+
 
 //Constructor function WORKS
-// function Book (author, title, noOfPages, isRead) {
-// this.author = author;
-// this.title = title;
-// this.noOfPages = noOfPages;
-// this.isRead = isRead;
+function Book (author, title, noOfPages, isRead) {
+this.author = author;
+this.title = title;
+this.noOfPages = noOfPages;
+this.isRead = isRead;
 
-// }
+ }
 
 // const Book1 = new Book ('sdf', 'asdf', 6, true);
 // console.log(Book1);
@@ -28,10 +46,14 @@ newBookButton.addEventListener('click', revealForm);
 
 // }
 
-submitButton.addEventListener('click', preventSubmit);
+addToLibraryButton.addEventListener('click', preventSubmit);
 function preventSubmit (event)
 {event.preventDefault();}
 
+addToLibraryButton.addEventListener('click', captureInputs);
+
+
+//                                                          
 
 
 
