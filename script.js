e6286e37717183395dc5c78a3264d2006431a17e
CodeCara <149534506+CodeCara@users.noreply.github.com>
2024-10-03 19:39:59 +0200

    All functionality plus CSS implemented

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const form = document.querySelector('form');
 //classnames not presented correctly!
 const addToLibraryButton = document.querySelector('.add-to-library-button');
 const libraryContainer = document.querySelector('.library-container');
-
+const clearAllFieldsButton = document.querySelector('.clear-all-fields-button');
 
 
 
@@ -66,28 +66,74 @@ function displayBooks (newBook) {
 
     libraryContainer.innerHTML=" ";
 
+    let colLabel1 = document.createElement('div');
+            let colLabel2 = document.createElement('div');
+            let colLabel3 = document.createElement('div');
+            let emptyDiv1 = document.createElement('div');
+            let emptyDiv2 = document.createElement('div');
+            colLabel1.style.fontWeight='bold';
+            colLabel2.style.fontWeight='bold';
+            colLabel3.style.fontWeight='bold';
+            emptyDiv1.style.fontWeight='bold';
+            emptyDiv2.style.fontWeight='bold';
+
+
+
+
+
+
+            colLabel1.innerText='Author';
+            colLabel2.innerText='Title';
+            colLabel3.innerText='No. of pages';
+            colLabel3.style.justifySelf='center';
+            emptyDiv1.innerText=' ';
+            emptyDiv2.innerText=' ';
+
+            document.querySelector('.library-container').appendChild(colLabel1);
+            document.querySelector('.library-container').appendChild(colLabel2);
+            document.querySelector('.library-container').appendChild(colLabel3);
+            document.querySelector('.library-container').appendChild(emptyDiv1);
+            document.querySelector('.library-container').appendChild(emptyDiv2);
+
+
+
     for (let i=0; i<myLibrary.length; i++)
    
         // {let bookDisplay =  myLibrary[i];
           { 
-            
+        
+
+
+
+
             let cardDiv1 = document.createElement('div');
             let cardDiv2 = document.createElement('div');
             let cardDiv3 = document.createElement('div');
           
             let deleteButton = document.createElement('button');
             let changeReadStatus = document.createElement('button');
-           
+           deleteButton.style.width='6rem';
+           deleteButton.style.alignSelf='right';
+           changeReadStatus.style.width='6rem';
+           changeReadStatus.style.alignSelf='right';
             
             
 
             cardDiv1.innerText=myLibrary[i].author;
             cardDiv2.innerText=myLibrary[i].title;
             cardDiv3.innerText=myLibrary[i].numberOfPages;
-            
 
-            //Data attribute for delete button functionality
-            // let indexNumber = i;
+            cardDiv1.style.alignSelf='center';
+            cardDiv2.style.alignSelf='center';  
+            cardDiv3.style.alignSelf='center';
+
+
+
+
+            cardDiv3.style.justifySelf='center';
+        
+
+          
             
           
             changeReadStatus.innerText=myLibrary[i].readStatus;
@@ -150,15 +196,22 @@ changeReadStatus.style.backgroundColor='red';
 
   }      
 
-     
-
  
-//      
+
+
+clearAllFieldsButton.addEventListener('click', preventSubmit);
+// function preventSubmit (event)
+// {event.preventDefault();}
+     
+clearAllFieldsButton.addEventListener('click', function () {
+  author.value =' '
+  title.value = ' '
+  numberOfPages.value = ' '
+});  
 
 
 
-
-    }
+} 
 
   
             // deleteButton.addEventListener('click', myLibrary.splice(1, [i]));
